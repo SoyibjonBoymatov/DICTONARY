6ecd9b1d19a16edc2e9c52f55947334c12e02811
Khan <soyibjon196@gmail.com>
2024-12-13 14:44:44 +0900

    Add files via upload

---
+++ another codes.py
@@ -0,0 +1,50 @@
+#student={
+#    "name":"Abu Zarr",
+  #  "age": 24,
+  #  "major":"literature",
+  #  "university":"oxford"
+  #  }
+#for a, b in student.items():
+   # print(f"a:{a}")
+  #  print(f"b:{b}")
+  
+phones={
+        "Abu bakr": "Iphone 16",
+        "Bilol":"Iphone 15 pro max",
+        "Umar Ibn Al-Khattab": "Samsung 24",
+        "Abu": "Iphone 16"
+        }
+#for a,b in phones.items():
+  #  print(f"{a}`s phone is {b}")
+  
+#products={"apple":6,
+ #        "kiwi":5,
+ #        "orange":8,
+#         "melon":32
+#         }
+#print("Products in store:")
+#for product in products.keys():
+ #   print(f"{product.title()} ")
+ # #  
+#market=["kiwi","apple", "orange", "melon", "watermelon"]
+#for product in products:
+#        print("The products we have:")
+#        print(f"{product} {products[product]}$")
+
+#for items in market:
+  #  if items not in products:
+       # print(f"Please, bring {items} too")
+
+#print("Products in the store:")
+#for product in sorted(products):
+#   print(product.title())
+
+#print("consumers use these phones:")
+#for phone in phones.values():
+  #  print(phone)
+
+#print("Consumers use these phones:")
+#for phone in set(phones.values()):
+ #   print(phone)
+ 
+#toys={"ball","car","doll", "car", "bear", "doll"}
